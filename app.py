885985d95b82d96b3c675f9b7db358315c21810f
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2024-05-18 23:14:21 -0700

    c

---
+++ app.py
@@ -0,0 +1,29 @@
+# app.py
+from flask import Flask, Response, render_template
+import cv2
+
+app = Flask(__name__)
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)  # Use 0 for the default camera
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
